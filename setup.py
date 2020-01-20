1322546a9ee8d16d264e22e8b1a7fdc9fa4cd90f
Mark Douthwaite <mark@douthwaite.io>
2020-01-20 21:54:26 +0000

    Changed versioning.

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ import os
 
 from setuptools import find_packages, setup
 
+from rtk8s import __version__
+
 
 MANIFEST_DIRS = ["rtk8s/templates"]
 # .rtk8s_apps/v1/default/src
@@ -28,7 +30,7 @@ build_manifest()
 
 setup(
     name="rtk8s",
-    version="0.0.5",
+    version=__version__,
     packages=find_packages(exclude=["contrib", "test-docs", "tests*"]),
     install_requires=["click", "pyyaml"],
     include_package_data=True,
